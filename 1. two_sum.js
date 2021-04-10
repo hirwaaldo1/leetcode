ebbf20d2f8608318d12cc20724404765cacfa34e
Hirwa Aldo <51756396+hirwaaldo1@users.noreply.github.com>
2021-04-10 15:09:46 +0200

    Create 1. two_sum.js

---
+++ 1. two_sum.js
@@ -0,0 +1,8 @@
+var twoSum = function(nums, target) {
+   return [0,1]
+};
+//Input: nums = [2,7,11,15], target = 9
+//Output: [0,1] 
+//which two numbers in this array you can add to get 9 but the to return them you need to use index useing that array
+//[2,7,11,15] numbers
+//[0,1,2,3,4] each index of those numbers
